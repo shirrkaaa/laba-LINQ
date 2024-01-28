33a258864237a6ba2e40ad5a32c2cf38928338a4
shirrkaaa <128734559+shirrkaaa@users.noreply.github.com>
2024-01-28 18:20:48 +0100

    Update QueryHelper.cs

--- ConsoleApp/ConsoleApp/QueryHelper.cs
+++ ConsoleApp/ConsoleApp/QueryHelper.cs
@@ -26,36 +26,70 @@ public class QueryHelper : IQueryHelper
             .Where(delivery => delivery.ClientId == clientId)
             .Select(delivery => new DeliveryShortInfo
             {
-                // Map properties accordingly
-                Id = d.Id,
+                Id = delivery.Id,
+                StartCity = delivery.Direction.Origin,
+                EndCity = delivery.Direction.Destination,
+                ClientId = delivery.ClientId,
+                Type = delivery.Type,
+                LoadingPeriod = delivery.LoadingPeriod,
+                ArrivalPeriod = delivery.ArrivalPeriod,
+                CargoType = delivery.CargoType
+                Status = delivery.Status,
                 ClientName = d.ClientName,
-                // Other properties...
-            }); //TODO: Завдання 3
+            }); // Завдання 3
     
     /// <summary>
     /// Get first ten Deliveries that starts at specified city and have specified type
     /// </summary>
-    public IEnumerable<Delivery> DeliveriesByCityAndType(IEnumerable<Delivery> deliveries, string cityName, DeliveryType type) => new List<Delivery>();//TODO: Завдання 4
+    public IEnumerable<Delivery> DeliveriesByCityAndType(IEnumerable<Delivery> deliveries, string cityName, DeliveryType type) =>
+        deliveries
+            .Where(delivery => delivery.Direction.Origin.City == cityName && delivery.Type == type)
+            .Take(10); //Завдання 4
     
     /// <summary>
     /// Order deliveries by status, then by start of loading period
     /// </summary>
-    public IEnumerable<Delivery> OrderByStatusThenByStartLoading(IEnumerable<Delivery> deliveries) => new List<Delivery>();//TODO: Завдання 5
+    public IEnumerable<Delivery> OrderByStatusThenByStartLoading(IEnumerable<Delivery> deliveries) =>
+        deliveries
+            .OrderBy(delivery => delivery.Status)
+            .ThenBy(delivery => delivery.StartLoadingPeriod); // Завдання 5
 
     /// <summary>
     /// Count unique cargo types
     /// </summary>
-    public int CountUniqCargoTypes(IEnumerable<Delivery> deliveries) => 0; //TODO: Завдання 6
+    public int CountUniqCargoTypes(IEnumerable<Delivery> deliveries) =>
+        deliveries.Select(delivery => delivery.CargoType).Distinct().Count(); // Завдання 6
     
     /// <summary>
     /// Group deliveries by status and count deliveries in each group
     /// </summary>
-    public Dictionary<DeliveryStatus, int> CountsByDeliveryStatus(IEnumerable<Delivery> deliveries) => new();//TODO: Завдання 7
+    public Dictionary<DeliveryStatus, int> CountsByDeliveryStatus(IEnumerable<Delivery> deliveries) =>
+        deliveries
+            .GroupBy(delivery => delivery.Status)
+            .ToDictionary(group => group.Key, group => group.Count()); // Завдання 7
     
     /// <summary>
     /// Group deliveries by start-end city pairs and calculate average gap between end of loading period and start of arrival period (calculate in minutes)
     /// </summary>
-    public IEnumerable<AverageGapsInfo> AverageTravelTimePerDirection(IEnumerable<Delivery> deliveries) => new List<AverageGapsInfo>();//TODO: Завдання 8
+    public IEnumerable<AverageGapsInfo> AverageTravelTimePerDirection(IEnumerable<Delivery> deliveries) =>
+        deliveries.GroupBy(delivery => new { StartCity = delivery.Direction.Origin, EndCity = delivery.Direction.Destination })
+            .Select(group => new AverageGapsInfo
+            {
+                StartCity = group.Key.Origin,
+                EndCity = group.Key.Destination,
+                AverageGap = group.Average(delivery => 
+            {
+                DateTime start = delivery.LoadingPeriod.Start.GetValueOrDefault();
+                DateTime end = delivery.ArrivalPeriod.End.GetValueOrDefault();
+
+                if (start != default && end != default)
+                {
+                    return (end - start).TotalMinutes;
+                }
+
+                return 0.0;
+            });
+        }); // Завдання 8
 
     /// <summary>
     /// Paging helper
